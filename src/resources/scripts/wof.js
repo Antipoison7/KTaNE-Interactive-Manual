e7c5ef4a21b33a7e2f65982e1eaa45074fa50695
Antipoison7 <orders.connor@gmail.com>
2025-06-16 13:38:21 +1000

    Feature: Added highlighting to words in advanced wof also made table alphabetical

---
+++ src/resources/scripts/wof.js
@@ -0,0 +1,114 @@
+<table>
+    <tr id="ready">
+        <th>"READY"</th>
+        <td>YES, OKAY, <span class="highlight-t">WHAT</span>, MIDDLE, LEFT, PRESS, RIGHT, BLANK, READY, NO, FIRST, <span class="highlight-t">UHHH</span>, NOTHING, WAIT</td>
+    </tr>
+    <tr id="first">
+        <th>"FIRST"</th>
+        <td>LEFT, OKAY, YES, MIDDLE, NO, RIGHT, NOTHING, <span class="highlight-t">UHHH</span>, WAIT, READY, BLANK, <span class="highlight-t">WHAT</span>, PRESS, FIRST</td>
+    </tr>
+    <tr id="no">
+        <th>"NO"</th>
+        <td>BLANK, <span class="highlight-t">UHHH</span>, WAIT, FIRST, <span class="highlight-t">WHAT</span>, READY, RIGHT, YES, NOTHING, LEFT, PRESS, OKAY, NO, MIDDLE</td>
+    </tr>
+    <tr id="blank">
+        <th>"BLANK"</th>
+        <td>WAIT, RIGHT, OKAY, MIDDLE, BLANK, PRESS, READY, NOTHING, NO, <span class="highlight-t">WHAT</span>, LEFT, <span class="highlight-t">UHHH</span>, YES, FIRST</td>
+    </tr>
+    <tr id="nothing">
+        <th>"NOTHING"</th>
+        <td><span class="highlight-t">UHHH</span>, RIGHT, OKAY, MIDDLE, YES, BLANK, NO, PRESS, LEFT, <span class="highlight-t">WHAT</span>, WAIT, FIRST, NOTHING, READY</td>
+    </tr>
+    <tr id="yes">
+        <th>"YES"</th>
+        <td>OKAY, RIGHT, <span class="highlight-t">UHHH</span>, MIDDLE, FIRST, <span class="highlight-t">WHAT</span>, PRESS, READY, NOTHING, YES, LEFT, BLANK, NO, WAIT</td>
+    </tr>
+    <tr id="what">
+        <th>"WHAT"</th>
+        <td><span class="highlight-t">UHHH</span>, <span class="highlight-t">WHAT</span>, LEFT, NOTHING, READY, BLANK, MIDDLE, NO, OKAY, FIRST, WAIT, YES, PRESS, RIGHT</td>
+    </tr>
+    <tr id="uhhh">
+        <th>"<span class="highlight-t">UHHH</span>"</th>
+        <td>READY, NOTHING, LEFT, <span class="highlight-t">WHAT</span>, OKAY, YES, RIGHT, NO, PRESS, BLANK, <span class="highlight-t">UHHH</span>, MIDDLE, WAIT, FIRST</td>
+    </tr>
+    <tr id="left">
+        <th>"LEFT"</th>
+        <td>RIGHT, LEFT, FIRST, NO, MIDDLE, YES, BLANK, <span class="highlight-t">WHAT</span>, <span class="highlight-t">UHHH</span>, WAIT, PRESS, READY, OKAY, NOTHING</td>
+    </tr>
+    <tr id="right">
+        <th>"RIGHT"</th>
+        <td>YES, NOTHING, READY, PRESS, NO, WAIT, <span class="highlight-t">WHAT</span>, RIGHT, MIDDLE, LEFT, <span class="highlight-t">UHHH</span>, BLANK, OKAY, FIRST</td>
+    </tr>
+    <tr id="middle">
+        <th>"MIDDLE"</th>
+        <td>BLANK, READY, OKAY, <span class="highlight-t">WHAT</span>, NOTHING, PRESS, NO, WAIT, LEFT, MIDDLE, RIGHT, FIRST, <span class="highlight-t">UHHH</span>, YES</td>
+    </tr>
+    <tr id="okay">
+        <th>"OKAY"</th>
+        <td>MIDDLE, NO, FIRST, YES, <span class="highlight-t">UHHH</span>, NOTHING, WAIT, OKAY, LEFT, READY, BLANK, PRESS, <span class="highlight-t">WHAT</span>, RIGHT</td>
+    </tr>
+    <tr id="wait">
+        <th>"WAIT"</th>
+        <td><span class="highlight-t">UHHH</span>, NO, BLANK, OKAY, YES, LEFT, FIRST, PRESS, <span class="highlight-t">WHAT</span>, WAIT, NOTHING, READY, RIGHT, MIDDLE</td>
+    </tr>
+    <tr id="press">
+        <th>"PRESS"</th>
+        <td>RIGHT, MIDDLE, YES, READY, PRESS, OKAY, NOTHING, <span class="highlight-t">UHHH</span>, BLANK, LEFT, FIRST, <span class="highlight-t">WHAT</span>, NO, WAIT</td>
+    </tr>
+    <tr id="you">
+        <th>"YOU"</th>
+        <td>SURE, <span class="highlight-t">YOU ARE</span>, <span class="highlight-t">YOUR</span>, <span class="highlight-t">YOU'RE</span>, NEXT, <span class="highlight-t">UH HUH</span>, <span class="highlight-t">UR</span>, HOLD, <span class="highlight-t">WHAT?</span>, <span class="highlight-t">YOU</span>, <span class="highlight-t">UH UH</span>, LIKE, DONE, <span class="highlight-t">U</span></td>
+    </tr>
+    <tr id="you are">
+        <th>"<span class="highlight-t">YOU ARE</span>"</th>
+        <td><span class="highlight-t">YOUR</span>, NEXT, LIKE, <span class="highlight-t">UH HUH</span>, <span class="highlight-t">WHAT?</span>, DONE, <span class="highlight-t">UH UH</span>, HOLD, <span class="highlight-t">YOU</span>, <span class="highlight-t">U</span>, <span class="highlight-t">YOU'RE</span>, SURE, <span class="highlight-t">UR</span>, <span class="highlight-t">YOU ARE</span></td>
+    </tr>
+    <tr id="your">
+        <th>"<span class="highlight-t">YOUR</span>"</th>
+        <td><span class="highlight-t">UH UH</span>, <span class="highlight-t">YOU ARE</span>, <span class="highlight-t">UH HUH</span>, <span class="highlight-t">YOUR</span>, NEXT, <span class="highlight-t">UR</span>, SURE, <span class="highlight-t">U</span>, <span class="highlight-t">YOU'RE</span>, <span class="highlight-t">YOU</span>, <span class="highlight-t">WHAT?</span>, HOLD, LIKE, DONE</td>
+    </tr>
+    <tr id="you're">
+        <th>"<span class="highlight-t">YOU'RE</span>"</th>
+        <td><span class="highlight-t">YOU</span>, <span class="highlight-t">YOU'RE</span>, <span class="highlight-t">UR</span>, NEXT, <span class="highlight-t">UH UH</span>, <span class="highlight-t">YOU ARE</span>, <span class="highlight-t">U</span>, <span class="highlight-t">YOUR</span>, <span class="highlight-t">WHAT?</span>, <span class="highlight-t">UH HUH</span>, SURE, DONE, LIKE, HOLD</td>
+    </tr>
+    <tr id="ur">
+        <th>"<span class="highlight-t">UR</span>"</th>
+        <td>DONE, <span class="highlight-t">U</span>, <span class="highlight-t">UR</span>, <span class="highlight-t">UH HUH</span>, <span class="highlight-t">WHAT?</span>, SURE, <span class="highlight-t">YOUR</span>, HOLD, <span class="highlight-t">YOU'RE</span>, LIKE, NEXT, <span class="highlight-t">UH UH</span>, <span class="highlight-t">YOU ARE</span>, YOU</td>
+    </tr>
+    <tr id="u">
+        <th>"<span class="highlight-t">U</span>"</th>
+        <td><span class="highlight-t">UH HUH</span>, SURE, NEXT, <span class="highlight-t">WHAT?</span>, <span class="highlight-t">YOU'RE</span>, <span class="highlight-t">UR</span>, <span class="highlight-t">UH UH</span>, DONE, <span class="highlight-t">U</span>, <span class="highlight-t">YOU</span>, LIKE, HOLD, <span class="highlight-t">YOU ARE</span>, <span class="highlight-t">YOUR</span></td>
+    </tr>
+    <tr id="uh-huh">
+        <th>"<span class="highlight-t">UH HUH</span>"</th>
+        <td><span class="highlight-t">UH HUH</span>, <span class="highlight-t">YOUR</span>, <span class="highlight-t">YOU ARE</span>, <span class="highlight-t">YOU</span>, DONE, HOLD, <span class="highlight-t">UH UH</span>, NEXT, SURE, LIKE, <span class="highlight-t">YOU'RE</span>, <span class="highlight-t">UR</span>, <span class="highlight-t">U</span>, <span class="highlight-t">WHAT?</span></td>
+    </tr>
+    <tr id="uh-uh">
+        <th>"<span class="highlight-t">UH UH</span>"</th>
+        <td><span class="highlight-t">UR</span>, <span class="highlight-t">U</span>, <span class="highlight-t">YOU ARE</span>, <span class="highlight-t">YOU'RE</span>, NEXT, <span class="highlight-t">UH UH</span>, DONE, <span class="highlight-t">YOU</span>, <span class="highlight-t">UH HUH</span>, LIKE, <span class="highlight-t">YOUR</span>, SURE, HOLD, <span class="highlight-t">WHAT?</span></td>
+    </tr>
+    <tr id="whatqmark">
+        <th>"<span class="highlight-t">WHAT?</span>"</th>
+        <td><span class="highlight-t">YOU</span>, HOLD, <span class="highlight-t">YOU'RE</span>, <span class="highlight-t">YOUR</span>, <span class="highlight-t">U</span>, DONE, <span class="highlight-t">UH UH</span>, LIKE, <span class="highlight-t">YOU ARE</span>, <span class="highlight-t">UH HUH</span>, <span class="highlight-t">UR</span>, NEXT, <span class="highlight-t">WHAT?</span>, SURE</td>
+    </tr>
+    <tr id="done">
+        <th>"DONE"</th>
+        <td>SURE, <span class="highlight-t">UH HUH</span>, NEXT, <span class="highlight-t">WHAT?</span>, <span class="highlight-t">YOUR</span>, <span class="highlight-t">UR</span>, <span class="highlight-t">YOU'RE</span>, HOLD, LIKE, <span class="highlight-t">YOU</span>, <span class="highlight-t">U</span>, <span class="highlight-t">YOU ARE</span>, <span class="highlight-t">UH UH</span>, DONE</td>
+    </tr>
+    <tr id="next">
+        <th>"NEXT"</th>
+        <td><span class="highlight-t">WHAT?</span>, <span class="highlight-t">UH HUH</span>, <span class="highlight-t">UH UH</span>, <span class="highlight-t">YOUR</span>, HOLD, SURE, NEXT, LIKE, DONE, <span class="highlight-t">YOU ARE</span>, <span class="highlight-t">UR</span>, <span class="highlight-t">YOU'RE</span>, <span class="highlight-t">U</span>, <span class="highlight-t">YOU</span></td>
+    </tr>
+    <tr id="hold">
+        <th>"HOLD"</th>
+        <td><span class="highlight-t">YOU ARE</span>, <span class="highlight-t">U</span>, DONE, <span class="highlight-t">UH UH</span>, <span class="highlight-t">YOU</span>, <span class="highlight-t">UR</span>, SURE, <span class="highlight-t">WHAT?</span>, <span class="highlight-t">YOU'RE</span>, NEXT, HOLD, <span class="highlight-t">UH HUH</span>, <span class="highlight-t">YOUR</span>, LIKE</td>
+    </tr>
+    <tr id="sure">
+        <th>"SURE"</th>
+        <td><span class="highlight-t">YOU ARE</span>, DONE, LIKE, <span class="highlight-t">YOU'RE</span>, <span class="highlight-t">YOU</span>, HOLD, <span class="highlight-t">UH HUH</span>, <span class="highlight-t">UR</span>, SURE, <span class="highlight-t">U</span>, <span class="highlight-t">WHAT?</span>, NEXT, <span class="highlight-t">YOUR</span>, <span class="highlight-t">UH UH</span></td>
+    </tr>
+    <tr id="like">
+        <th>"LIKE"</th>
+        <td><span class="highlight-t">YOU'RE</span>, NEXT, <span class="highlight-t">U</span>, <span class="highlight-t">UR</span>, HOLD, DONE, <span class="highlight-t">UH UH</span>, <span class="highlight-t">WHAT?</span>, <span class="highlight-t">UH HUH</span>, <span class="highlight-t">YOU</span>, LIKE, SURE, <span class="highlight-t">YOU ARE</span>, <span class="highlight-t">YOUR</span></td>
+    </tr>
+</table>
